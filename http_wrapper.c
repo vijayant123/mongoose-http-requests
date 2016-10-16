b4a84122653f7f66629d4d1f5d11e0d14ebcee1a
Vijayant <vijayant.saini123@gmail.com>
2016-10-16 16:14:25 +0530

    creeated basic wrapper

--- http_wrapper.c
+++ http_wrapper.c
@@ -1,4 +1,11 @@
-// mongoose wrapper for http requests
+/**
+ *
+ * created by vijayant saini @vijayant123
+ *
+ * mongoose wrapper for http requests
+ *
+ * todo make this wrapper MT-safe
+*/
 
 #include <stddef.h>
 
@@ -8,31 +15,40 @@
 // header for this wrapper
 
 
-typedef struct http_output {
+typedef struct http_response {
     int response_code;
     const char *response_message;
     size_t response_length;
-} response;
+} res;
+
+typedef struct http_request {
+    const char *url;
+    const char *extra_headers;
+    const char *post_data;
+} req;
 
 
 // global state variables
 
-response *response_ptr;
+res *response_ptr;
+void (*response_callback)();
+
+// global mg_ structures
+
+struct mg_mgr request_mgr;
 
 
 #ifndef HEADER_LIBS_INCLUDED
 
-#include "../libs/mongoose.c"
+#include "../../libs/mongoose.c"
 
 #define HEADER_LIBS_INCLUDED
 #endif
 
 
 void connect(
-        char * output_buffer,
-        void ** success_callback,
         struct mg_mgr *mgr,
-        mg_event_handler_t ev_callback
+        mg_event_handler_t ev_callback,
         const char *url,
         const char *extra_headers,
         const char *post_data
@@ -41,16 +57,16 @@ void connect(
     mg_connect_http(mgr, ev_callback, url, extra_headers, post_data);
 }
 
-void handle_return(){
-    if()
+void handle_return() {
+    response_callback();
 }
 
 static void ev_callback(
         struct mg_connection *conn,
         int ev,
         void *p
-){
-    if(ev == MG_EV_HTTP_REPLY){
+) {
+    if (ev == MG_EV_HTTP_REPLY) {
         struct http_message *hm = (struct http_message *) p;
 
         response_ptr->response_code = hm->resp_code;
@@ -61,3 +77,13 @@ static void ev_callback(
     }
 }
 
+void get(
+        req *request,
+        void (*callback(void)),
+        res *response
+) {
+    response_callback = callback;
+    response_ptr =  response;
+
+    connect(&request_mgr, ev_callback, request->url, request->extra_headers, request->post_data);
+}
